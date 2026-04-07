94e5791dfd8e9194b23968e10efa5d6ac00865e4
jimvak <73341186+jimvak@users.noreply.github.com>
2026-04-07 12:09:30 +0300

    Create eda_temperature_yearly_subplots.py

---
+++ src/eda_temperature_yearly_subplots.py
@@ -0,0 +1,64 @@
+import csv
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+data = pd.read_csv('test.csv')
+
+temp = data['mean_temp']
+
+
+mean_temp = temp.to_numpy()
+
+final_mean_temp = []
+
+for x in mean_temp:
+    if x=='---':
+        final_mean_temp.append(-1)
+    else:
+        final_mean_temp.append(float(x))
+
+
+    
+
+
+k = 0
+
+etos = 2010
+
+for i in range(10):
+    fig, axs = plt.subplots(2, 2)
+    axs[0, 0].plot(final_mean_temp[k:90+k])
+    axs[0, 0].set_title('Proto Trimino '+str(etos))
+    axs[0, 1].plot(final_mean_temp[91+k:182+k], 'tab:orange')
+    axs[0, 1].set_title('Deytero Trimino ' + str(etos))
+    axs[1, 0].plot(final_mean_temp[183+k:273+k], 'tab:green')
+    axs[1, 0].set_title('Trito Trimino ' + str(etos))
+    axs[1, 1].plot(final_mean_temp[274+k:365+k], 'tab:red')
+    axs[1, 1].set_title('Tetarto Trimino ' + str(etos))
+    
+    for ax in axs.flat:
+        ax.set(xlabel='Day', ylabel='Mean Temp')
+    
+    # Hide x labels and tick labels for top plots and y ticks for right plots.
+    for ax in axs.flat:
+        ax.label_outer()
+    k=k+365
+    etos=etos+1
+
+
+
+
+
+
+
+
+
+
+    
+        
+ 
